7b952ed1bccf65008e055f1877e77845215ba295
DESKTOP-VC20UED\MostafaAbdelazeem <mostafa.abdo.ch12@gmail.com>
2021-04-27 16:13:49 +0200

    Fix Primary title in homepage in all screens and fix hero logo in blog page

---
+++ js/blog.js
@@ -0,0 +1,5 @@
+let myBody = document.querySelector('body');
+myBody.classList.add('blogPostCopy');
+if(document.body.classList.contains('blogPostCopy')){
+   $('header .logo img').attr("src","img/V-beta.svg");
+}
