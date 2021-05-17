def6c2ab4a59beada9491df19ab579220bbb49fd
MostafaAbdelazeem <mostafa.azeem@viola.ae>
2021-05-17 14:20:46 +0200

    update

--- js/contact.js
+++ js/contact.js
@@ -1,7 +1,7 @@
 let myBody = document.querySelector('body');
 myBody.classList.add('contactPage');
 if (document.body.classList.contains('contactPage')) {
-    $('header > .logo img').attr('src', 'img/V-beta-white.svg');
+    $('header > .logo img').attr('src', 'img/V-logo-white.svg');
     $('header > .withLove').attr('src', 'img/withLoveGray.svg');
     $('.social li a img ').removeClass('invertColor')
 
