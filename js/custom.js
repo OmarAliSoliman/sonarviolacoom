8df2126df920a430e404d4ec1051f6932fea6af0
Omar Soliman <omar.soliman@viola.ae>
2023-05-21 17:18:02 +0300

    new blog

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+$(window).load(function () {
+
+  $(".blog .blog-list").niceScroll({
+      cursorcolor: "#301464",
+      cursorwidth: "5px",
+      cursorborder: "none",
+      cursorborderradius: "5px",
+      autohidemode: false,
+      railpadding: { top: 0, right: 3, left: 3, bottom: 0 }
+    });
+});
