c14c7225af7ecae21a81ddfc9b4d93f46bbe5f3d
Omar Soliman <omar.soliman@viola.ae>
2023-07-26 17:24:58 +0300

    coments

--- contact_mail.php
+++ contact_mail.php
@@ -64,7 +64,7 @@
       
         
         $mail->CharSet = 'UTF-8';
-        $mail->addAddress("marketing@viola.ae", 'Viola Communication'); 
+        $mail->addAddress("omar.soliman@viola.ae", 'Viola Communication'); 
          
          
         $mail->isHTML(true); 
