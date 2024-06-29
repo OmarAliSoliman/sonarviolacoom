e5e3f382e259e2e7495dc2e6b0a2761bd8f9e45e
OmarAliSoliman <omarasmar9955@gmail.com>
2024-06-29 16:14:53 +0300

    viola image new page

--- js/csrpage.js
+++ js/csrpage.js
@@ -1,8 +1,8 @@
-let myBody = document.querySelector('body');
-myBody.classList.add('csrPage');
-if (document.body.classList.contains('csrPage')) {
-    $('header > .logo img').attr('src', 'img/V-logo-white.svg');
-    $('header > .withLove').attr('src', 'img/withLoveGray.svg');
-    $('.social li a img ').removeClass('invertColor')
-
+let myBody = document.querySelector("body");
+myBody.classList.add("csrPage");
+if (document.body.classList.contains("csrPage")) {
+  $("header .logo img").attr("src", "img/V-logo.svg");
+  // $('header > .logo img').attr('src', 'img/V-logo-white.svg');
+  $("header > .withLove").attr("src", "img/withLoveGray.svg");
+  $(".social li a img ").addClass("invertColor");
 }
